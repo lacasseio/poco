5858551c7a60d211bc81736dae5e65a159718faf
Francis ANDRE <zosrothko@orange.fr>
2016-08-11 12:08:50 +0200

    Add token() method
    Signed-off-by: Francis ANDRE <zosrothko@orange.fr>

--- FSM/src/Operation.cpp
+++ FSM/src/Operation.cpp
@@ -18,9 +18,44 @@ Operation::Operation(Operator op) :_operator(op)
 Operation:: ~Operation()
 {
 }
+const string Operation::token() const
+{
+	string result;
+	switch(_operator) {
+	case		NONE: result = "";break;
+	case		ADD: result = "+"; break;			// +
+	case		SUB: result = "-"; break;			// -
+	case		MULT: result = "*"; break;			// *
+	case		DIV: result = "/"; break;			// /
+	case		POW: result = "**"; break;			// **
+	case		BITXOR: result = "^"; break;			// ^
+	case		BITAND: result = "&"; break;			// &
+	case		BITOR: result = "|"; break;			// |
+	case		AND: result = "&&"; break;			// &&
+	case		OR: result = "||"; break;				// ||
+	case		NOT: result = "!"; break;			// !
+	case		EQUAL: result = "=="; break;			// ==
+	case		NOTEQUAL: result = "!="; break;		// !=
+	case		LESS: result = "<"; break;			// <
+	case		LESSOREQ: result = "<="; break;		// <=
+	case		GREATER: result = ">"; break;		// >
+	case		GREATEROREQ: result = ">="; break;	// >=
+	case		SELECTOR: result = "."; break;		// .
+	case		ACCESSOR: result = "->"; break;		// ->
+	case		OPENPAR: result = "("; break;		// (
+	case		CLOSPAR: result = ")"; break;			// )
+	default: result = "???";
+	}
+	return result;
+}
+
+bool Operation::precede(const Operation& op) const
+{
+	return true;
+}
 void Operation::setNoContextuel()
 {
-    if (_operator == equal || _operator == notequal)
+    if (_operator == EQUAL || _operator == NOTEQUAL || _operator == SELECTOR)
     {
         _contextuel = false;
     }
