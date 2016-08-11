6891b510a5cceea72044801c0eae692e0e086184
Francis ANDRE <zosrothko@orange.fr>
2016-08-11 12:08:50 +0200

    Fix precedence computation
    Signed-off-by: Francis ANDRE <zosrothko@orange.fr>

--- FSM/src/Operation.cpp
+++ FSM/src/Operation.cpp
@@ -48,11 +48,34 @@ const string Operation::token() const
 	}
 	return result;
 }
+//
+// operator's precedence.
+/*
+1 	()   []   ->   .   :: 										Function call, scope, array/member access
+2 	 !   ~   -   +   *   &   sizeof   type cast   ++   --   	(most) unary operators, sizeof AND type casts (right to left)
+3 	*   /   % MOD 												Multiplication, division, modulo
+4 	+   - 														Addition AND subtraction
+5 	<<   >> 													Bitwise shift left AND right
+6 	<   <=   >   >= 											Comparisons: less-than, ...
+7 	==   != 													Comparisons: EQUAL AND not EQUAL
+8 	& 															Bitwise	AND
+9 	^ 															Bitwise exclusive OR (XOR)
+10 	| 															Bitwise inclusive (normal) OR
+11 	&& 															Logical AND
+12 	||															Logical OR
+13 	 ? : 														Conditional expression (ternary)
+14 	=   +=   -=   *=   /=   %=   &=   |=   ^=   <<=   >>= 		Assignment operators (right to left)
+15 	, 															Comma operator
+*/
+//
 
-bool Operation::precede(const Operation& op) const
+bool Operation::precede(const Operation* op) const
 {
-	return true;
+	const int left = precedences[this->op()];
+	const int right = precedences[op->op()];
+	return left >= right;
 }
+
 void Operation::setNoContextuel()
 {
     if (_operator == EQUAL || _operator == NOTEQUAL || _operator == SELECTOR)
